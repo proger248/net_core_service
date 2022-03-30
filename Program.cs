97e0616bebf5039a77cd2d7fa3b19dd1dbf0341d
VadikCherepanov <proger248@yandex.ru>
2022-03-30 13:33:58 +0300

    Initialized commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Service
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var builder = new HostBuilder()
+				.ConfigureAppConfiguration(confBuilder => 
+				{
+					confBuilder.AddJsonFile("config.json");
+					confBuilder.AddCommandLine(args);
+				})
+				.ConfigureLogging((configLogging) =>
+				{
+					configLogging.AddConsole();
+					configLogging.AddDebug();
+				})
+				.ConfigureServices((services) =>
+				{
+					
+				});
+
+
+			await builder.RunConsoleAsync();
+		}
+	}
+}
---
+++ Services/TaskSchedulerService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+	public class TaskSchedulerService : IHostedService, IDisposable
+	{
+		private Timer timer;
+
+		public TaskSchedulerService(IServiceProvider services)
+		{
+
+		}
+
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			timer = new Timer(
+				(e) => ProcessTask(),
+				null,
+				TimeSpan.Zero,
+				TimeSpan.FromSeconds(10));
+
+			return Task.CompletedTask;
+		}
+
+		private void ProcessTask()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
